ad99d32c87efd24daf2955002c741b549e926789
George Baroi <baroi.george@gmail.com>
2022-11-09 20:37:08 -0500

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(driver, string){
+    return driver.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === string.toLowerCase()
+    )
+};
+
+function fuzzyMatch(driver, string){
+    return driver.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0 
+    )
+}
+
+function matchName(driver, string){
+    return driver.filter(
+        (record) => record.name === string 
+    )
+}
